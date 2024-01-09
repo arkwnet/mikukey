99eefb06a8004edad73bfdcc8f978871db01c83c
Sora Arakawa <mail@arkw.net>
2024-01-09 12:59:54 +0900

    Delete Misskey.py

--- MiAPI.py
+++ MiAPI.py
@@ -1,12 +1,35 @@
 import datetime
-import MiConfig as config
+import json
+import urllib.error
+import urllib.request
+import MiConfig
 
 def timeline(since_id):
-    return config.api.notes_hybrid_timeline(
-        limit = 10,
-        since_id = since_id
-    )
+    url = "https://" + MiConfig.host + "/api/notes/timeline"
+    data = {
+        "i": MiConfig.token,
+        "limit": 10
+    }
+    headers = {
+        "Content-Type": "application/json",
+        "User-Agent": "mozilla/5.0"
+    }
+    req = urllib.request.Request(url, data = json.dumps(data).encode("utf-8"), headers = headers, method = "POST")
+    res = urllib.request.urlopen(req)
+    body = res.read()
+    return json.loads(body)
 
 def post(text):
-    config.api.notes_create(text = text)
+    url = "https://" + MiConfig.host + "/api/notes/create"
+    data = {
+        "i": MiConfig.token,
+        "text": text
+    }
+    headers = {
+        "Content-Type": "application/json",
+        "User-Agent": "mozilla/5.0"
+    }
+    req = urllib.request.Request(url, data = json.dumps(data).encode("utf-8"), headers = headers, method = "POST")
+    res = urllib.request.urlopen(req)
+    body = res.read()
     return
