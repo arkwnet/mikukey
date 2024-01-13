5409ec3719b2a4ac1ac67b813b783f2dcf4664c0
Sora Arakawa <mail@arkw.net>
2024-01-13 15:05:11 +0900

    Add call function

--- MiAPI.py
+++ MiAPI.py
@@ -3,21 +3,14 @@ import json
 import urllib.error
 import urllib.request
 import MiConfig
+USER_AGENT = "mozilla/5.0"
 
 def timeline(since_id):
-    url = "https://" + MiConfig.host + "/api/notes/timeline"
     data = {
         "i": MiConfig.token,
         "limit": 10
     }
-    headers = {
-        "Content-Type": "application/json",
-        "User-Agent": "mozilla/5.0"
-    }
-    req = urllib.request.Request(url, data = json.dumps(data).encode("utf-8"), headers = headers, method = "POST")
-    res = urllib.request.urlopen(req)
-    body = res.read()
-    return json.loads(body)
+    return call("notes/timeline", data)
 
 def post(text):
     url = "https://" + MiConfig.host + "/api/notes/create"
@@ -25,23 +18,20 @@ def post(text):
         "i": MiConfig.token,
         "text": text
     }
-    headers = {
-        "Content-Type": "application/json",
-        "User-Agent": "mozilla/5.0"
-    }
-    req = urllib.request.Request(url, data = json.dumps(data).encode("utf-8"), headers = headers, method = "POST")
-    res = urllib.request.urlopen(req)
-    body = res.read()
+    call("notes/create", data)
     return
 
 def i():
-    url = "https://" + MiConfig.host + "/api/i"
     data = {
         "i": MiConfig.token
     }
+    return call("i", data)
+
+def call(api, data):
+    url = "https://" + MiConfig.host + "/api/" + api
     headers = {
         "Content-Type": "application/json",
-        "User-Agent": "mozilla/5.0"
+        "User-Agent": USER_AGENT
     }
     req = urllib.request.Request(url, data = json.dumps(data).encode("utf-8"), headers = headers, method = "POST")
     res = urllib.request.urlopen(req)
@@ -50,7 +40,7 @@ def i():
 
 def download(url, path):
     headers = {
-        "User-Agent": "mozilla/5.0"
+        "User-Agent": USER_AGENT
     }
     req = urllib.request.Request(url, headers = headers)
     with urllib.request.urlopen(req) as file_web:
