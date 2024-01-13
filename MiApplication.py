6f2080f19e6d63d4295b30eb883086a271d39bd2
Sora Arakawa <mail@arkw.net>
2024-01-13 12:29:38 +0900

    Change to same layout as web client

--- MiApplication.py
+++ MiApplication.py
@@ -76,24 +76,24 @@ class Application(tkinter.Frame):
         photo = tkinter.PhotoImage(file = "./assets/icon.png")
         self.master.iconphoto(False, photo)
 
-        self.profile_canvas = tkinter.Canvas(self.master, bg = "#757575")
-        self.profile_canvas.place(x = 6, y = 6, w = 48, h = 48)
-        self.profile_name = tkinter.Label(self.master, font = ("sans-serif", "12", "bold"), fg = "#000000")
-        self.profile_name.place(x = 60, y = 12)
-        self.profile_id = tkinter.Label(self.master, font = ("sans-serif", "10"), fg = "#757575")
-        self.profile_id.place(x = 60, y = 30)
-
-        self.note_entry = scrolledtext.ScrolledText(self.master)
-        self.note_entry.place(x = 250, y = 0, w = MiConfig.WIDTH - 350, h = 60)
-        self.note_button_post = tkinter.Button(self.master, text = "ノート", command = self.post)
-        self.note_button_post.place(x = MiConfig.WIDTH - 100, y = 30, w = 100, h = 30)
-
         self.timeline = tkinter.ttk.Treeview(self.master, columns = (1, 2, 3, 4), show = "")
         self.timeline.column(1, width = 200, anchor = "nw")
         self.timeline.column(2, width = 200, anchor = "nw")
         self.timeline.column(3, width = 400, anchor = "nw")
         self.timeline.column(4, width = 200, anchor = "nw")
-        self.timeline.place(x = 0, y = 60, w = MiConfig.WIDTH, h = MiConfig.HEIGHT - 60)
+        self.timeline.place(x = 0, y = 0, w = MiConfig.WIDTH, h = MiConfig.HEIGHT - 60)
+
+        self.profile_canvas = tkinter.Canvas(self.master, bg = "#757575")
+        self.profile_canvas.place(x = 6, y = MiConfig.HEIGHT - 54, w = 48, h = 48)
+        self.profile_name = tkinter.Label(self.master, font = ("sans-serif", "12", "bold"), fg = "#000000")
+        self.profile_name.place(x = 60, y = MiConfig.HEIGHT - 48)
+        self.profile_id = tkinter.Label(self.master, font = ("sans-serif", "10"), fg = "#757575")
+        self.profile_id.place(x = 60, y = MiConfig.HEIGHT - 30)
+
+        self.note_entry = scrolledtext.ScrolledText(self.master)
+        self.note_entry.place(x = 250, y = MiConfig.HEIGHT - 60, w = MiConfig.WIDTH - 350, h = 60)
+        self.note_button_post = tkinter.Button(self.master, text = "ノート", command = self.post)
+        self.note_button_post.place(x = MiConfig.WIDTH - 100, y = MiConfig.HEIGHT - 30, w = 100, h = 30)
 
         self.loop()
         self.master.mainloop()
